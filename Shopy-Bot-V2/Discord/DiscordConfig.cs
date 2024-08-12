ae942a2b02ba2a19a2182c0ecd0fe4a37e6997a3
StartCPany <xcvjkh348s@outlook.com>
2024-08-12 22:07:30 +0400

    Upload Files

---
+++ Shopy-Bot-V2/Discord/DiscordConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Shared;
+
+namespace Discord;
+public class DiscordConfig : DefaultConfig
+{
+    [JsonPropertyName("token")]
+    public string Token { get; set; }
+    [JsonPropertyName("prod")]
+    public bool Production { get; set; }
+    [JsonPropertyName("prefix")]
+    public string[] Prefix { get; set; }
+    [JsonPropertyName("jacobubikey")]
+    public string JacobUBIKey { get; set; }
+}
