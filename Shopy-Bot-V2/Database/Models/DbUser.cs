e971ce48b090b6481e9d0fed9aea514b7ffab90b
StartCPany <xcvjkh348s@outlook.com>
2024-08-12 22:07:18 +0400

    Upload Files

---
+++ Shopy-Bot-V2/Database/Models/DbUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Lilia.Database.Models;
+
+public class DbUser
+{
+    public ulong DbUserId { get; set; }
+    public ulong DiscordUserId { get; set; }
+    public string OsuMode { get; set; }
+    public string OsuUsername { get; set; }
+}
