1156cb80780fdc1712ed7513ac1ce3f8d39a9434
StartCPany <xcvjkh348s@outlook.com>
2024-08-12 22:07:13 +0400

    Upload Files

---
+++ Shopy-Bot-V2/Database/Extensions/GuildDbAccess.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using DSharpPlus.Entities;
+using Lilia.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lilia.Database.Extensions;
+
+public static class GuildDbAccess
+{
+    public static DbGuild GetOrCreateGuildRecord(this LiliaDatabaseContext ctx, DiscordGuild discordGuild)
+    {
+        DbSet<DbGuild> guilds = ctx.Guilds;
+        DbGuild guild = guilds.FirstOrDefault(entity => entity.DiscordGuildId == discordGuild.Id);
+
+        if (guild == default(DbGuild))
+        {
+            guild = new DbGuild
+            {
+                DiscordGuildId = discordGuild.Id
+            };
+
+            guilds.Add(guild);
+        }
+
+        ctx.SaveChanges();
+        return guild;
+    }
+}
