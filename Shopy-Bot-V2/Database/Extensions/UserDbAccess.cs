2e0693231909649c4e5ee484838ec26584021ea1
StartCPany <xcvjkh348s@outlook.com>
2024-08-12 22:07:14 +0400

    Upload Files

---
+++ Shopy-Bot-V2/Database/Extensions/UserDbAccess.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using DSharpPlus.Entities;
+using Lilia.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lilia.Database.Extensions;
+
+public static class UserDbAccess
+{
+    public static DbUser GetOrCreateUserRecord(this LiliaDatabaseContext ctx, DiscordUser discordUser)
+    {
+        DbSet<DbUser> users = ctx.Users;
+        DbUser user = users.FirstOrDefault(entity => entity.DiscordUserId == discordUser.Id);
+
+        if (user == default(DbUser))
+        {
+            user = new DbUser
+            {
+                DiscordUserId = discordUser.Id,
+                OsuMode = string.Empty,
+                OsuUsername = string.Empty
+            };
+
+            users.Add(user);
+        }
+
+        ctx.SaveChanges();
+        return user;
+    }
+}
